db6dbe6f9e9ee8b61b3afd9eaedae4dcccafec67
ML510 <mr.mahesh.303@gmail.com>
2026-07-28 21:22:38 +0530

    add prime location showcase component

---
+++ src/app/components/prime-location/prime-location.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prime-location',
+  standalone: true,
+  imports: [],
+  templateUrl: './prime-location.html',
+  styleUrl: './prime-location.css',
+})
+export class PrimeLocation {
+
+}
